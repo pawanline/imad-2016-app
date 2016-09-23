fa7239afd634afe39d0e6c597c6c409c5f494744
Pawan kumar <pawanline@gmail.com>
2016-09-23 22:30:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,8 +25,7 @@ button.onclick = function(){
     };
 
 //Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     //make a request to the server
@@ -53,6 +52,8 @@ submit.onclick = function(){
     };
    
     //make the request
+    var nameInput = document.getElementById('name');
+var name = nameInput.value;
     request.open('GET','http://pawanline.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
    
