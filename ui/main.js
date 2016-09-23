bcd767da20108d9053e5377bd53d98687eacd115
Pawan kumar <pawanline@gmail.com>
2016-09-23 10:31:23 +0530

    [imad-console] Updates ui/main.js

---
+++ ui/main.js
@@ -0,0 +1,40 @@
+//counter code
+var button = document.getElementById('counter');
+button.onClick = function(){
+    
+
+//create a request object
+var request = new XMLHttpRequest();
+//capture the response and store it in a variable
+request.onreadystatechange = function(){
+if(request.readyState === XMLHttpRequest.DONE){
+//Take some action
+if(request.status ===200){
+var counter = request.responseText;
+span.innerHTML = counter.toString();
+}
+}
+//Not done yet
+};
+
+//make the request
+request.open('GET',"http://pawanline.imad.hasura-app.io/counter",true);
+request.send(null);
+};
+//submit name
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+//make a request to the server and send the name
+//capture a list of names and render it as a list
+var names = ['name1','name2','name3','name4'];
+var list = "";
+for(var i = 0;i<names.length;i++){
+list += "<li>" + names[i] + "</li>";
+}
+
+var ul = document.getElementById('namelist');
+ul.innerHTML = list;
+};
