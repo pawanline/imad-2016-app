4d6f14c0df06362c8a20681687866d462b9c7a21
Pawan kumar <pawanline@gmail.com>
2016-09-23 09:17:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,48 +1,147 @@
-//counter code
-var button = document.getElementById('counter') ;
-
-button.onclick = function(){
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+var articles = {
+'article-one' : {
+    title:"Article One|Pawan kumar",
+    heading:"Article One",
+    date:'5 sep 2016',
+    content:`
+    <P>
+            This is the content for my first article.i am writing it @3am on 19-9-16-
+        </P>
+        
+        <P>
+            This is the content for my first article.i am writing it @3am on 19-9-16-
+        </P>
+        
+         <P>
+            This is the content for my first article.i am writing it @3am on 19-9-16-
+        </P>`
+  
+},
+'article-two' : {
+    title:"Article Two|Pawan kumar",
+    heading:"Article Two",
+    date:'5 sep 2016',
+    content:`
+    <P>
+            This is the content for my second article.i am writing it @3am on 19-9-16-
+        </P>
+        
+        <P>
+            This is the content for my second article.i am writing it @3am on 19-9-16-
+        </P>
+        
+         <P>
+            This is the content for my second article.i am writing it @3am on 19-9-16-
+        </P>`
+    
+},
+'article-three' : {
+    title:"Article Three|Pawan kumar",
+    heading:"Article Three",
+    date:'5 sep 2016',
+    content:`
+    <P>
+            This is the content for my third article.i am writing it @3am on 19-9-16-
+        </P>
+        
+        <P>
+            This is the content for my third article.i am writing it @3am on 19-9-16-
+        </P>
+        
+         <P>
+            This is the content for my third article.i am writing it @3am on 19-9-16-
+        </P>`
+    
+},
+};
+function createTemplate(data){
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
     
 
+var htmlTemplate = `<html>
+<head>
+    <title> ${title}</title>
+    <meta name = "viewport" content="width-device-width,initial-scale-1"/>
+    <style>
+        .container{
+     max-width: 800px;
+    margin: 0 auto;
+    font-size: 32px;
+    color: #5f565f;
+    font-family: sans-serif;
+    padding-left: 20px;
+    padding-right: 20px;
+        }
+    </style>
+</head>
 
-            //create a request object
-            var request = new XMLHttpRequest();
-            
-            //counter the response and store it in a variable
-           request.onreadystatechange = function(){
-              if(request.readyState === XMLHttpRequest.DONE)  {
-                  //Take some action
-                  if(request.status === 200){
-                      var counter = request.responseText;
-                       
-            var update = document.getElementById('count');
-            update.innerHTML = counter.toString();
-                  }
-              }
-              //not done yet
-              
-            };
-            //make the request
-            request.open('GET','http://pawanline.imad.hasura-app.io/counter',true);
-            request.send(null);
-           
-    
-};
+<body>
+    <div class="container">
+    <div>
+        <a href="/">Home</a>
+    </div>
+    <hr/>
+    <h3>
+        ${heading}
+    </h3>
+    <div>
+      ${date};
+    </div>
+    <div>
+       
+        ${content}
+        
+    </div>
+    </div>
+</body>
+</html>
+`;
+return htmlTemplate;
+}
 
-//submit name
-var nameInput=document.getElementById('name');
-var name = nameInput.value;
-var submit = document.getElementById('submit_btn');
-submit.onclick = function (){
-  //make a request to the server and send the name
-  //capture a list of name and render it as a list
-  
-  var names = ['name1','name2','name3','name4'];
-  var list ="";
-  for (var i=0; i<names.length;i++){
-      list += "<li>"+names[i]+"</li>";
-  }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
-  
-};
+var app = express();
+app.use(morgan('combined'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+var counter = 0;
+app.get('/counter',function(req,res){
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+
+app.get('/:articleName',function(req,res){
+   // articleName == article-one
+    //article[articleName] == {} content object for article one
+    var articleName = req.params.articleName;
+     res.send(createTemplate(articles[articleName]));
+});
+
+
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/main.js',function(req,res){
+    res.sendFile(path.join(__dirname,'ui','main.js'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+var names=[];
+
+
+
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
