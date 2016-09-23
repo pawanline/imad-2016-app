2dee6bc9c80024738c93d75024a5b4bee09b534a
Pawan kumar <pawanline@gmail.com>
2016-09-23 22:24:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,11 +18,11 @@ button.onclick = function(){
         }
         //not done yet
     };
+   
     //make the request
     request.open('GET','http://pawanline.imad.hasura-app.io/counter',true);
     request.send(null);
-    
-};
+    };
 
 //Submit name
 var nameInput = document.getElementById('name');
@@ -30,13 +30,30 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     //make a request to the server
+     //create  a request object
+    var request = new XMLHttpRequest();
     
-    //Capture a list of names  and read it  as a list
-    var names = ['name1','name2','name3','name4'];
+    //capture the  response and store it  in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState ===XMLHttpRequest.DONE){
+            //Take some Action
+            if(request.status === 200){
+              //Capture a list of names  and read it  as a list
+    var names = request.responseText;
+    names = JSON.parse(names);
     var list = "";
     for(var i = 0;i<names.length;i++){
         list += "<li>"+names[i]+"</li>";
     }
     var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    ul.innerHTML = list;   
+            }
+        }
+        //not done yet
+    };
+   
+    //make the request
+    request.open('GET','http://pawanline.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
+   
 };
