ba7236a6edea57365d74360eedf61bba00a1f8c6
Pawan kumar <pawanline@gmail.com>
2016-09-23 21:56:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,64 +1,25 @@
 //counter code
-var button = document.getElementById("counter");
+var button = document.getElementById('counter');
+
 button.onclick = function(){
     
-
-//create a request object
-var request = new XMLHttpRequest();
-//capture the response and store it in a variable
-    request.onreadystatechange = function(){
-    if(request.readyState === XMLHttpRequest.DONE){
-    //Take some action
-    if(request.status ===200){
-    var counter = request.responseText;
-    span.innerHTML = counter.toString();
-        }
+    //create  a request object
+    var request = new XMLHttpRequest();
     
-    }
-            //Not done yet
+    //capture the  response and store it  in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState ===XMLHttpRequest.DONE){
+            //Take some Action
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        //not done yet
     };
-
-//make the request
-request.open('GET',"http://pawanline.imad.hasura-app.io/counter",true);
-request.send(null);
-};
-
-
-//submit name
-
-var nameInput = document.getElementById("name");
-var name = nameInput.value;
-var submit = document.getElementById("submit_btn");
-
-submit.onclick = function(){
-
-//create a request object
-var request = new XMLHttpRequest();
-//capture the response and store it in a variable
-    request.onreadystatechange = function(){
-    if(request.readyState === XMLHttpRequest.DONE){
-    //Take some action
-    if(request.status ===200){
-        //capture  a list of names and render it as a list
-    var names = request.responseText;
-    names = JSON.parse(names);
-    var list ="";
-    for (var i = 0;i<names.length;i++){
-        list += "<li"+names[i]+"</li>";
-    }
+    //make the request
+    request.open('GET','http://pawanline.imad.hasura-app.io/counter',true);
+    request.send(null);
     
-    var ul = document.getElementById("namelist");
-    ul.innerHTML = list;
-        }
-    
-        
-    }
-            //Not done yet
-    
-    
-};
-
-//make the request
-request.open('GET',"http://pawanline.imad.hasura-app.io/submit-name?name="+name,true);
-request.send(null);
 };
