22d75628f309b905da4675855814372f853faca3
Pawan kumar <pawanline@gmail.com>
2016-11-19 01:58:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,200 +12,143 @@ var config = {
     port:'5432',
     password:process.env.DB_PASSWORD
 };
-var articles = {
-'article-one' : {
-    title:"Contact me",
-    heading:"click on below links to reach me",
-    date:'5 sep 2016',
+
+var app = express();
+app.use(morgan('combined'));
+
+var articles={
+    'article-one':{
+    title : 'Article One | Venkatesh',
+    heading: 'Article One',
+    date: 'Sep 05, 2016',
     content:`
-    <span>
-           <a href="https://www.linkedin.com/in/pawan-kumar-430951111?trk=nav_responsive_tab_profile_pic"/> <img  src ="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" width="80" height="80" border="0" />
-        </span>
-        
-        <span>
-    <a href="https://twitter.com/pawanku18045433"/><img src ="https://upload.wikimedia.org/wikipedia/en/thumb/9/9f/Twitter_bird_logo_2012.svg/1259px-Twitter_bird_logo_2012.svg.png" width="80" height="80"/>
-        </span>
-        
-         <p>
-            <a href="<a href="https://www.facebook.com/profile.php?id=100004177570711" /><img src ="https://www.underconsideration.com/brandnew/archives/facebook_2015_logo_detail.png" width="100" height="100"/>
-        </p>
-        `
-  
+                <p>
+                    This is my first article
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque laoreet lectus ac augue ornare, sed facilisis enim tempor. Sed venenatis leo non orci interdum, ut ullamcorper quam lacinia. Nullam eget rhoncus leo, ac pellentesque felis. Cras egestas aliquet augue, nec viverra orci egestas id. Sed nec tortor at tortor tincidunt volutpat eu at ipsum. Integer euismod euismod est, mattis aliquet quam finibus vel. Fusce venenatis lectus nec laoreet ultricies. Aliquam erat volutpat. Vestibulum eget libero augue. 
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pulvinar felis eget eros accumsan porttitor. Morbi posuere feugiat diam vitae egestas. In et semper metus. Praesent feugiat nec turpis ut consequat. Ut condimentum mi leo, et malesuada nunc sagittis sed. Donec cursus ornare ante, non sodales lacus bibendum facilisis. Nulla quis leo placerat, molestie risus eu, cursus mi. Maecenas quis leo dictum, dignissim mauris et, accumsan libero. Integer nec ullamcorper est, quis ornare dui. Etiam in pulvinar turpis, vitae semper quam. 
+                </p>`
 },
-'article-two' : {
-    title:"Article Two|Pawan kumar",
-    heading:"Article Two",
-    date:'5 sep 2016',
+    'article-two':{
+    title : 'Article Two | Venkatesh',
+    heading: 'Article Two',
+    date: 'Sep 06, 2016',
     content:`
-    <P>
-            This is the content for my second article.i am writing it @3am on 19-9-16-
-        </P>
-        
-        <P>
-            This is the content for my second article.i am writing it @3am on 19-9-16-
-        </P>
-        
-         <P>
-            This is the content for my second article.i am writing it @3am on 19-9-16-
-        </P>`
-    
+                <p>
+                    This is my first article
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque laoreet lectus ac augue ornare, sed facilisis enim tempor. Sed venenatis leo non orci interdum, ut ullamcorper quam lacinia. Nullam eget rhoncus leo, ac pellentesque felis. Cras egestas aliquet augue, nec viverra orci egestas id. Sed nec tortor at tortor tincidunt volutpat eu at ipsum. Integer euismod euismod est, mattis aliquet quam finibus vel. Fusce venenatis lectus nec laoreet ultricies. Aliquam erat volutpat. Vestibulum eget libero augue. 
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pulvinar felis eget eros accumsan porttitor. Morbi posuere feugiat diam vitae egestas. In et semper metus. Praesent feugiat nec turpis ut consequat. Ut condimentum mi leo, et malesuada nunc sagittis sed. Donec cursus ornare ante, non sodales lacus bibendum facilisis. Nulla quis leo placerat, molestie risus eu, cursus mi. Maecenas quis leo dictum, dignissim mauris et, accumsan libero. Integer nec ullamcorper est, quis ornare dui. Etiam in pulvinar turpis, vitae semper quam. 
+                </p>`
 },
-'article-three' : {
-    title:"Aticle Three|Pawan kumar",
-    heading:"Article Three",
-    date:'5 sep 2016',
+    'article-three':{
+    title : 'Article Three | Venkatesh',
+    heading: 'Article Three',
+    date: 'Sep 07, 2016',
     content:`
-    <P>
-            This is the content for my third article.i am writing it @3am on 19-9-16-
-        </P>
-        
-        <P>
-            This is the content for my third article.i am writing it @3am on 19-9-16-
-        </P>
-        
-         <P>
-            This is the content for my third article.i am writing it @3am on 19-9-16-
-        </P>`
-    
-},
-'Project':{
-    title:"My Project",
-    heading:"My Project",
-    date:'22 sep 2016',
-    content:`
-    <p> Time table Application on android platform</p>
-    <p> This is an application to improve communication between teacher and student in an effective way.Teacher can scheduled their class as per theri convenience and student will get to know through notification through the app</p>
-    `
-    
+                <p>
+                    This is my first article
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque laoreet lectus ac augue ornare, sed facilisis enim tempor. Sed venenatis leo non orci interdum, ut ullamcorper quam lacinia. Nullam eget rhoncus leo, ac pellentesque felis. Cras egestas aliquet augue, nec viverra orci egestas id. Sed nec tortor at tortor tincidunt volutpat eu at ipsum. Integer euismod euismod est, mattis aliquet quam finibus vel. Fusce venenatis lectus nec laoreet ultricies. Aliquam erat volutpat. Vestibulum eget libero augue. 
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pulvinar felis eget eros accumsan porttitor. Morbi posuere feugiat diam vitae egestas. In et semper metus. Praesent feugiat nec turpis ut consequat. Ut condimentum mi leo, et malesuada nunc sagittis sed. Donec cursus ornare ante, non sodales lacus bibendum facilisis. Nulla quis leo placerat, molestie risus eu, cursus mi. Maecenas quis leo dictum, dignissim mauris et, accumsan libero. Integer nec ullamcorper est, quis ornare dui. Etiam in pulvinar turpis, vitae semper quam. 
+                </p>`
 }
 };
+/*
+var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+  pool.query('SELECT * FROM article',function(err,result){
+      if(err){
+          res.status(500).send(err.toString());
+      }else{
+          res.send(JSON.stringify(result));
+      }
+  });
+});
+*/
 
 function createTemplate(data){
-    var title = data.title;
+    var title=data.title;
+    var heading=data.heading;
     var date = data.date;
-    var heading = data.heading;
     var content = data.content;
     
-
-var htmlTemplate = `<html>
-<head>
-    <title> ${title}</title>
-    <meta name = "viewport" content="width-device-width,initial-scale-1"/>
-    <style>
-        .container{
-     max-width: 800px;
-    margin: 0 auto;
-    font-size: 32px;
-    color: #5f565f;
-    font-family: sans-serif;
-    padding-left: 20px;
-    padding-right: 20px;
-        }
-    </style>
-</head>
-<body>
+var htmlTemplate=`<html>
+    <head>
+        <title>${title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
     <div class="container">
-    <div>
-        <a href="/">Home</a>
-    </div>
-    <hr/>
-    <h3>
-        ${heading}
-    </h3>
-    <div>
-      ${date};
-    </div>
-    <div>
-       
-        ${content}
-        
-    </div>
-    </div>
-</body>
+        <div>
+            <a href="/">Home</a>
+            <hr />
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+                ${content}
+            </div>
+        </div>
+        </div>
+    </body>
 </html>
 `;
 return htmlTemplate;
 }
 
-var app = express();
-app.use(morgan('combined'));
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-}); 
-var pool = new Pool(config);
-app.get('/test-db',function(req,res){
-    //make a select request 
-    //return  a response with the results
-    pool.query('SELECT * FROM test',function(err,result){
-       if(err){
-           res.status(500).send(err.toString());
-       } 
-       else{
-           res.send(JSON.stringify(result));
-       }
-    });
-    
 });
 
-app.get('/articles/:articlename', function (req, res) {
-    pool.query("SELECT * FROM article WHERE title='"+req.params.articlename+"'",function(err,result){
-        if(err)
-        {
-            res.status(500).send(err.tostring());
-        }
-        if(result.rows.length===0)
-        {
-            res.status(404).send(err.tosrting());
-        }
-        else
-        {
-            var articledata=result.rows[0];
-            res.send(createtemplate(articledata));
-        }    
-    });
-});
-var counter = 0;
+var counter=0;
 app.get('/counter',function(req,res){
-    counter = counter + 1;
-    res.send(counter.toString());
+	counter=counter+1;
+	res.send(counter.toString());
 });
+
 var names=[];
-app.get('/submit-name',function(req,res){
-                                                    //URL:/submit-name?name=xxxx
-    //get the name from the request
-    var name =req.query.name;//todo
-    names.push(name);
-    //JSON 
-    res.send(JSON.stringify(names)); //todo
-    
+app.get('/submit-name/',function(req,res){
+    var name = req.query.name;
+	names.push(name);
+	res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName',function(req,res){
-   // articleName == article-one
-    //article[articleName] == {} content object for article one
-    var articleName = req.params.articleName;
-     res.send(createTemplate(articles[articleName]));
+app.get('/:articleName', function (req, res) {
+  //res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
 });
-
 
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/main.js',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','main.js'));
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/profileimage.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'profileimage.jpg'));
 });
-
-
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+Contact GitHub 
