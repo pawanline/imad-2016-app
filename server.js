cc8d518265a872f57f7758e68ec796b0d1079121
Pawan kumar <pawanline@gmail.com>
2016-12-22 06:26:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-require('bootstrap');
 
 var config = {
     user:'pawanline',
