6f9313e4086fdd2698fcfd2cd31269259ff8c0cf
Pawan kumar <pawanline@gmail.com>
2016-12-30 18:26:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'bootstrap-Example Implementation/docs/examples/starter-template', 'index.html'));
+  res.sendFile(path.join(__dirname, 'bootstrap-Example Implementation/docs/examples/theme', 'index.html'));
 });     
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
