fd59c0aa8819e5084183acaff57a8353c93b60a3
pawanline <pawanline@gmail.com>
2017-01-06 14:00:43 +0530

    my first commit

---
+++ myserver.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/',function(req,res){
+    //res.send('hello iam here');
+    res.sendFile(__dirname+'/index.html');
+    });
+
+app.listen('3000');
+console.log('listening on port 3000');
